39780ebb8aa460e2de728926b9c12132fb925f49
Furik Jan <xfurik00@eva.fit.vutbr.cz>
2024-10-27 10:45:37 +0100

    add index.html, get_conferences, style.css

--- get_conferences.php
+++ get_conferences.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 }
 
 // SQL dotaz pro načtení konferencí
-$sql = "SELECT name, start_datetime FROM conferences";
+$sql = "SELECT name, start_datetime, end_datetime FROM conferences";
 $result = $conn->query($sql);
 
 // Převedení výsledků do JSON formátu
