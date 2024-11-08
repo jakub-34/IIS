be47a596d2833708067b2e1527bffc8048debfe5
Jakub <jakubhrdlicka34@gmail.com>
2024-11-08 10:35:44 +0100

    feat: add admin page button

--- check_session.php
+++ check_session.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-$response = ["isLoggedIn" => isset($_SESSION['user_id'])];
+$response = [
+    "isLoggedIn" => isset($_SESSION['user_id']),
+    "role" => $_SESSION['role'] ?? null
+];
 header('Content-Type: application/json');
 echo json_encode($response);
 ?>
